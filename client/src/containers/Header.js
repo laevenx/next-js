0197696e1c0ea025bd4785e3ce3e26d6ee30c4ec
Grady Wicoady <nervalgravie@gmail.com>
2020-08-31 16:55:30 +0800

    improved header and  working list

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -11,7 +11,7 @@ export function Header(){
              <Menu theme="dark" mode="horizontal">
             <Menu.Item key="1"><Link href="/login"><a>Login</a></Link></Menu.Item>
             <Menu.Item key="2"><Link href="/register"><a>Register</a></Link></Menu.Item>
-            <Menu.Item key="3"><Link href="/shop/bread"><a>Products</a></Link></Menu.Item>
+            <Menu.Item key="3"><Link href="/list"><a>Products</a></Link></Menu.Item>
             <Menu.Item key="4"><Link href="/shop/checkout"><a>Checkout</a></Link></Menu.Item>
             </Menu>
         </div>
--- client/src/pages/list.js
+++ client/src/pages/list.js
@@ -1,26 +1,80 @@
+import {
+  Layout,
+  Menu,
+  Breadcrumb,
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Row,
+  Col,
+} from "antd";
 import Link from "next/link";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const { Content, Footer } = Layout;
+import { Header } from "../containers/Header";
 
 import { useEffect, useState } from "react";
 
-export default function List({ownersList}) {
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
 
+export default function List({ ownersList }) {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div>
-      {ownersList.map((e, index) => (
-        <div key={index}>
-          <Link as={`/${e.vehicle}/${e.ownerName}`} href="/[vehicle]/[person]">
-            <a>
-              Navigate to {e.ownerName}'s {e.vehicle}
-            </a>
-          </Link>
+    <Layout className="layout">
+      <Header></Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+        </Breadcrumb>
+        <div>
+          {ownersList.map((e, index) => (
+            <div key={index}>
+              <img
+                src="https://i2.wp.com/butterwithasideofbread.com/wp-content/uploads/2012/07/Easiest-Best-Homemade-Bread.BSB_.IMG_6014.jpg?w=775&ssl=1"
+                height="100"
+                width="125"
+              ></img>
+              <Link as={`/shop/${e.name}`} href="/shop/[person]">
+                <a>
+                  {e.name}'s stock: {e.amount} price: {e.price}
+                </a>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
 
 List.getInitialProps = async () => {
-    const response = await fetch("http://localhost:4001/vehicles");
-          const ownersList = await response.json();
-    return {ownersList: ownersList}
-}
+  const response = await fetch("http://localhost:3001/product");
+  const ownersList = await response.json();
+  return { ownersList: ownersList };
+};
